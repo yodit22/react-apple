41e4f8d9260a4e69f685ebf457ae11d53ced7a5d
yodit22 <judye320@gmail.com>
2023-04-24 21:40:23 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Components/Header";
+import Main from "./Components/Main/Main";
+import Footer from "./Components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+export default App;
---
+++ src/Components/Main/Main.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Alert from "../Alert";
+import IPadPro from "../IPadPro";
+import MacBookAir from "../MacBookAir";
+import IPhone11Pro from "../IPhone11Pro";
+import Iphone11 from "../Iphone11";
+import AppletvWatch from "../AppletvWatch";
+import ArcadeAppleCard from "../ArcadeAppleCard";
+
+function Main() {
+  return (
+    <div>
+      <Alert />
+      <IPadPro />
+      <MacBookAir />
+      <IPhone11Pro />
+      <Iphone11/>
+      <AppletvWatch />
+      <ArcadeAppleCard />
+    </div>
+  );
+}
+
+export default Main;
